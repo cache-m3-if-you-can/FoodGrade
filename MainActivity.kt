ba39beba9e7b1b0424e191941e9843946eb5bf26
Anwesha Pradhan <anweshapradhan2406@gmail.com>
2025-04-15 23:26:52 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.signup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.signup.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView (R.layout.activity_main)
+    }
+}
